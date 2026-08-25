efed6f80a3101826d56ad79055753914188bd2d9
Sidharth Sachdev <73532857+sidsachdev12@users.noreply.github.com>
2026-08-25 11:22:50 +0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) == 1:
+            return s1 in s2
+
+        # letters = set(s1)
+
+        # Sort s1
+        s1_sorted = sorted(s1)
+
+        # sliding window of size len(s1)
+        left = 0
+
+        # Check if the characters in window match characters in s1
+        for right in range(len(s1), len(s2) + 1):
+
+            window = s2[left:right]
+            # window_set = set(window)
+
+            # if not window_set - letters and not letters - window_set:
+            if sorted(window) == s1_sorted:
+                return True
+            
+            left += 1
+
+        # if loop end, return False
+        return False
